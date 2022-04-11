6153ed406fb590201fad41b76b72caf6049a449f
chentoufadnane <102356936+chentoufadnane@users.noreply.github.com>
2022-04-11 15:30:16 +0200

    Delete Stap 6 - Fileserver directory

--- Stap 6 - Fileserver/FS toevoegen aan het domein.ps1
+++
@@ -1,4 +0,0 @@
-$User = "demoAdmin"
-$PWord = ConvertTo-SecureString -String "demoTestPass123" -AsPlainText -Force
-$Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User, $PWord
-Add-Computer -ComputerName FS01 -DomainName scripttest.be -OUPath "OU=Servers,OU=VanRoey - Computers,OU=VanRoey,DC=demomarch,DC=be" -Credential $Credential -Restart -Force
