3a7023d271424e3bbc06af23a1fe40ed7cbd793e
chentoufadnane <102356936+chentoufadnane@users.noreply.github.com>
2022-05-02 16:27:44 +0200

    Delete Stage/Templates/1. Storage Account directory

--- Stage/Templates/1. Storage Account/01-storage.ps1
+++
@@ -1,5 +0,0 @@
-$rg = "AC-ARM"
-
-New-AzResourceGroup -Name $rg -Location "westeurope" -Force
-
-New-AzResourceGroupDeployment -Name "new-rg-storageacc-keyv" -ResourceGroupName $rg -TemplateFile "01-storage.json" -TemplateParameterFile "01-storage.parameters.json"
