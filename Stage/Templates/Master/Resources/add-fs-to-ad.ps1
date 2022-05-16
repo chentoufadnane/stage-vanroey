a32943d013e0ebf184916cc4e7ccc907a0c927f1
chentoufadnane <102356936+chentoufadnane@users.noreply.github.com>
2022-05-16 11:55:12 +0200

    Delete Stage directory

--- Stage/Templates/Master/Resources/add-fs-to-ad.ps1
+++
@@ -1,8 +0,0 @@
-$FSName = 'mstFS'
-$FSAdminUsername = 'mstAdmin'
-$FSAdminPassword = ConvertTo-SecureString -String "mstTempPass123" -AsPlainText -Force
-$domain = 'mstechnics.be'
-$oupath = 'OU=Servers,OU=MSTechnics - Computers,OU=MSTechnics,DC=mstechnics,DC=be'
-
-$Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $FSAdminUsername, $FSAdminPassword
-Add-Computer -ComputerName $FSName -DomainName $domain -OUPath $oupath -Credential $Credential -Restart -Force
